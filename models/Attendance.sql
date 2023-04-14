41cb5df8458e1f90e00a14e70b44dd05af83a3b7
Thang Nguyen <truongthang2211@gmail.com>
2023-04-14 16:22:03 +0700

    Update variable

--- models/Attendance.sql
+++ models/Attendance.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        pre_hook= delete_attendance_data(4)
+        pre_hook= delete_attendance_data( var('month') )
     )
 }}
 select uuid_generate_v4() as "Id", 
