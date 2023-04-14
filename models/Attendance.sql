7ed4c75dc6ffea7ef7f3794bc8fbd35289f50588
Thang Nguyen <truongthang2211@gmail.com>
2023-04-14 15:30:07 +0700

    Update tranformation for exel

--- models/Attendance.sql
+++ models/Attendance.sql
@@ -7,11 +7,11 @@
 select uuid_generate_v4() as "Id", 
       "Employ ID" as "UserId", 
       'Office' as "WorkType", 
-      MIN(TO_TIMESTAMP("Time",'MM/DD/YYYY HH24:MI') ) as "StartTime", 
-      MAX(TO_TIMESTAMP("Time",'MM/DD/YYYY HH24:MI') ) as "EndTime", 
+      MIN("Time"::timestamp) as "StartTime", 
+      MAX("Time"::timestamp) as "EndTime", 
       null as "CreatedBy", 
       null as "UpdatedBy", 
       Now() as "CreatedAt", 
       Now() as "UpdatedAt"
-from {{ source('attendances','attendance_csv') }}
-group by "Employ ID", DATE(TO_TIMESTAMP("Time",'MM/DD/YYYY HH24:MI') )
+from {{ source('attendances','attendance_excel') }}
+group by "Employ ID", DATE("Time"::timestamp)
